396a7b0c5b9f554712c50c9ce595ceb8d5b8d105
Dave Schilling <ds@Daves-MacBook-Pro.local>
2018-11-13 14:20:30 -0700

    mvp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import Form from "./components/Form";
 
 class App extends Component {
+  state = {
+    repos: null
+  }
+
+  getUser = (e) => {
+    e.preventDefault();
+    const user = e.target.elements.username.value;
+    if (user) {
+      axios.get(`https://api.github.com/users/${user}`)
+      .then((res) => {
+        const repos = res.data.public_repos;
+        this.setState({ repos });
+      })
+    } else return;
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Find your Repos number <a href="https://github.com/azdaves"><i className="fa fa-github-alt fa-2x"></i></a></h1>
         </header>
+        <Form getUser={this.getUser} />
+        { this.state.repos ? <p>Number of repos: { this.state.repos }</p> : <p>Please enter a username.</p> }
       </div>
     );
   }
